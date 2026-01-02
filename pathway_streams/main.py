c5e1d45e506331f2440d5dec9bb2aac9ed0f2435
code735 <prathmeshjkumbhar7777777@gmail.com>
2026-01-02 18:41:42 +0530

    added master csv

--- pathway_streams/main.py
+++ pathway_streams/main.py
@@ -5,7 +5,10 @@ from schema import RedditSentimentSchema
 print("Pathway version:", pw.__version__)
 print(os.getcwd())
 
-table = pw.io.jsonlines.read("./pathway_streams/data_stream/", schema=RedditSentimentSchema)
+table = pw.io.jsonlines.read(
+    "./pathway_streams/data_stream/",
+    schema=RedditSentimentSchema
+)
 
 latest_table = table.groupby(table.post_id).reduce(
     post_id=table.post_id,
@@ -13,6 +16,7 @@ latest_table = table.groupby(table.post_id).reduce(
     text=pw.reducers.latest(table.text),
 )
 
-pw.io.csv.write(table, "./output.csv")
-# pw.debug.compute_and_print(table)
+filtered_table = table.filter(table.ticker != "UNKNOWN")
+
+pw.io.csv.write(filtered_table, "./output.csv")
 pw.run()

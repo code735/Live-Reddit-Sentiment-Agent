43e030a1459dc8506a80230a71aabc372aafc508
code735 <prathmeshjkumbhar7777777@gmail.com>
2026-01-04 16:42:20 +0530

    converted schema from just file to module

--- pathway_streams/main.py
+++ pathway_streams/main.py
@@ -1,7 +1,6 @@
 import pathway as pw
 import os
 from .schema import RedditSentimentSchema
-from agent.main import on_new_rows
 
 print("Pathway version:", pw.__version__)
 print(os.getcwd())
@@ -20,5 +19,4 @@ latest_table = table.groupby(table.post_id).reduce(
 filtered_table = table.filter(table.ticker != "UNKNOWN")
 
 pw.io.csv.write(filtered_table, "./output.csv")
-pw.io.subscribe(filtered_table, on_new_rows)
 pw.run()

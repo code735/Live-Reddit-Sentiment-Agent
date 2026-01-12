1814166c69e8e7e94d81a4d09f5896152f193cfc
code735 <prathmeshjkumbhar7777777@gmail.com>
2026-01-12 19:53:01 +0530

    issue in /tickers/

--- server/routes/tickers.py
+++ server/routes/tickers.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
+from agent.main import read_csv_file_live
 
 router = APIRouter()
 
 @router.get("/")
 def get_users():
-    
-    return {"users": ["Alice", "Bob"]}
+  read_csv_file_live()
+  return {
+    "message": "successfully calculated sentiment and added in output"
+  }

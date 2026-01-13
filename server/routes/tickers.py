7d88781dc931af33a0cef2d029cc629d60e31d90
code735 <prathmeshjkumbhar7777777@gmail.com>
2026-01-13 19:44:48 +0530

    crawler route

--- server/routes/tickers.py
+++ server/routes/tickers.py
@@ -10,6 +10,13 @@ def get_users():
     "message": "successfully calculated sentiment and added in output"
   }
 
+@router.get("/crawler")
+def get_users():
+  read_csv_file_live()
+  return {
+    "message": "successfully calculated sentiment and added in output"
+  }
+
 
 
   # start crawler -> get output in output.csv -> then trigger read_csv_file_live()

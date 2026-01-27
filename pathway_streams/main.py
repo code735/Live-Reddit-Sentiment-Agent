f30acc219d44be0dfc7f17fbae3a2a7424250696
code735 <prathmeshjkumbhar7777777@gmail.com>
2026-01-27 19:52:42 +0530

    keeping only original entries

--- pathway_streams/main.py
+++ pathway_streams/main.py
@@ -4,22 +4,27 @@ from .schema import RedditSentimentSchema
 
 
 def start_pawthway():
-  print("Pathway version:", pw.__version__)
-  print(os.getcwd())
+    print("Pathway version:", pw.__version__)
+    print(os.getcwd())
 
-  table = pw.io.jsonlines.read(
-      "./pathway_streams/data_stream/", schema=RedditSentimentSchema
-  )
+    table = pw.io.jsonlines.read(
+        "./pathway_streams/data_stream/",
+        schema=RedditSentimentSchema,
+    )
 
-  latest_table = table.groupby(table.post_id).reduce(
-      post_id=table.post_id,
-      sentiment=pw.reducers.latest(table.sentiment),
-      text=pw.reducers.latest(table.text),
-  )
+    # Keep only the first time each post_id appears
+    deduplicated_table = table.groupby(table.post_id).reduce(
+        post_id=table.post_id,
+        sentiment=pw.reducers.earliest(table.sentiment),
+        text=pw.reducers.earliest(table.text),
+        ticker=pw.reducers.earliest(table.ticker),
+    )
 
-  filtered_table = table.filter(table.ticker != "UNKNOWN")
+    # Apply your filter AFTER deduplication
+    filtered_table = deduplicated_table.filter(
+        deduplicated_table.ticker != "UNKNOWN"
+    )
 
-  pw.io.csv.write(filtered_table, "./output.csv")
-  pw.run(
-    with_dashboard=False
-  )
+    pw.io.csv.write(filtered_table, "./output.csv")
+
+    pw.run(with_dashboard=False)
